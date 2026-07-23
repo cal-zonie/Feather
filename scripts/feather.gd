e12474667c9d918a5c666a69fb0357e196db2913
Andrew Li <andrewli6504@gmail.com>
2026-07-23 01:17:18 -0500

    feather is draggable

---
+++ scripts/feather.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+var dragging := false
+var mouse_offset := Vector2.ZERO
+var mouse_inside := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if dragging:
+		position = get_global_mouse_position() + mouse_offset
+
+func _on_mouse_entered():
+	mouse_inside = true;
+
+func _on_mouse_exited():
+	mouse_inside = false
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed and mouse_inside:
+			mouse_offset = position - get_global_mouse_position()
+			dragging = true
+		else:
+			dragging = false
--- scripts/hand.gd
+++ scripts/hand.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	#TODO: Move this to a manager class of some sort
+	get_viewport().physics_object_picking = true
+	get_viewport().physics_object_picking_sort = true
+	get_viewport().physics_object_picking_first_only = true
 	
 func _process(delta):
 	position = get_viewport().get_mouse_position()
