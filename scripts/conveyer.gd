04b1302c474c5a057d9dfb5a701aa70ccb594396
Andrew Li <andrewli6504@gmail.com>
2026-07-25 19:42:16 -0500

    box spawning and despawning

--- scripts/conveyer.gd
+++ scripts/conveyer.gd
@@ -2,6 +2,8 @@ extends Node
 
 @export var template_box: PackedScene
 
+signal on_submit_box(feather_count: int, required_feathers: int)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,5 +12,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("debug"):
-		var new_box = template_box.duplicate(true).instantiate()
-		$Boxes.add_child(new_box)
+		add_box()
+
+func add_box():
+	var new_box = template_box.duplicate(true).instantiate()
+	new_box.finish.connect(submit_box)
+	$Boxes.add_child(new_box)
+
+func submit_box(feather_count: int, required_feathers: int):
+	on_submit_box.emit(feather_count, required_feathers)
