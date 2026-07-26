487b8a87d30e22e1d3fd02c168aa730d6cf9f1ab
Andrew Li <andrewli6504@gmail.com>
2026-07-26 10:48:52 -0500

    random sprites

--- scripts/manager.gd
+++ scripts/manager.gd
@@ -23,9 +23,6 @@ func _process(delta):
 			wind = !wind
 			wind_timer = wind_max_timer if wind else wind_wait_timer
 			wind_speed *= -1 if wind else 1
-			print(wind)
-			print(wind_timer)
-			print(wind_speed)
 
 func get_conveyor_speed():
 	return conveyor_speed[current_level - 1]
